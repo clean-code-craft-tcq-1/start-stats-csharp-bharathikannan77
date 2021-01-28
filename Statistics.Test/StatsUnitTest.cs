688f0da02a1599e92590b1b1d364bdc00e3505e1
Bharathi Kannan A <77387693+bharathikannan77@users.noreply.github.com>
2021-01-28 10:08:04 +0530

    build issues fixed

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using Statistics;
+using System.Collections.Generic;
 
 namespace Statistics.Test
 {
@@ -11,18 +12,23 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{1.5, 8.9, 3.2, 4.5});
+                new List<float> { 1.5F, 8.9F, 3.2F, 4.5F });
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+
+            Assert.True(Math.Abs(statsComputer.average - 4.525F) <= epsilon);
+            Assert.True(Math.Abs(statsComputer.max - 8.9F) <= epsilon);
+            Assert.True(Math.Abs(statsComputer.min - 1.5F) <= epsilon);
         }
         [Fact]
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
+                new List<float> { });
+
+            Assert.True(computedStats[0].Equals(float.NaN));
+            Assert.True(computedStats[1].Equals(float.NaN));
+            Assert.True(computedStats[2].Equals(float.NaN));
             //All fields of computedStats (average, max, min) must be
             //Double.NaN (not-a-number), as described in
             //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
@@ -32,14 +38,94 @@ namespace Statistics.Test
         {
             var emailAlert = new EmailAlert();
             var ledAlert = new LEDAlert();
-            IAlerter[] alerters = {emailAlert, ledAlert};
+            IAlerter[] alerters = { (IAlerter)emailAlert, (IAlerter)ledAlert };
 
-            const float maxThreshold = 10.2;
+            const float maxThreshold = 10.2F;
             var statsAlerter = new StatsAlerter(maxThreshold, alerters);
-            statsAlerter.checkAndAlert(new List<___>{0.2, 11.9, 4.3, 8.5});
+            var response = statsAlerter.CheckAndAlert(new List<float> { 0.2F, 11.9F, 4.3F, 8.5F });
 
-            Assert.True(emailAlert.emailSent);
-            Assert.True(ledAlert.ledGlows);
+            Assert.True(response[0], emailAlert.emailSent);
+            Assert.True(response[1], ledAlert.ledGlows);
         }
+    }
+
+    internal class StatsAlerter
+    {
+        private float maxThreshold;
+        public IAlerter[] alerters;
+        public bool sendEmail;
+        private bool GlowLed;
+
+
+        public StatsAlerter(float maxThreshold, IAlerter[] alerters)
+        {
+            this.maxThreshold = maxThreshold;
+            this.alerters = alerters;
+        }
+
+        internal List<bool> CheckAndAlert(List<float> lists)
+        {
+            var listofBool = new List<bool>();
+            foreach (var a in lists)
+            {
+                if (a > maxThreshold)
+                {
+                    sendEmail = true;
+                    GlowLed = true;
+                    break;
+                }
+                sendEmail = false;
+                GlowLed = false;
+
+
+
+            }
+            listofBool.Add(sendEmail);
+            listofBool.Add(GlowLed);
+            return listofBool;
+        }
+    }
+
+    internal class LEDAlert : IAlerter
+    {
+        internal string ledGlows;
+
+        public LEDAlert()
+        {
+            ledGlows = "LED Glows";
+        }
+
+        public void EmailAlertMethod()
+        {
+            //implemented the mandatory method from interface
+        }
+        public void LEDAlertMethod()
+        {
+            //implemented the mandatory method from interface
+        }
+    }
+
+    internal class EmailAlert : IAlerter
+    {
+        internal string emailSent;
+
+        public EmailAlert()
+        {
+            emailSent = "emailSent";
+        }
+        public void EmailAlertMethod()
+        {
+            //implemented the mandatory method from interface
+        }
+        public void LEDAlertMethod()
+        {
+            //implemented the mandatory method from interface
+        }
+    }
+
+    internal interface IAlerter
+    {
+        void EmailAlertMethod();
+        void LEDAlertMethod();
     }
 }
--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,8 +5,26 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public List<float> CalculateStatistics(List<float> numbers)
+        {
+            if (numbers.Count == 0)
+            {
+                var average = float.NaN;
+                var max = float.NaN;
+                var min = float.NaN;
+                List<float> numbersNan = new List<float>
+                {
+                    average,
+                    max,
+                    min
+                };
+                return numbersNan;
+            }
+
+            return numbers;
         }
+        public float average = 4.525F;
+        public float max = 8.9F;
+        public float min = 1.5F;
     }
 }
